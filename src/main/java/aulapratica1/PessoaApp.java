409a4c0d3f0c9ada256a3f031ac7a8fc5dce1f7d
Estefanie <Estefanieaparecida0@gmail.com>
2021-07-01 15:36:20 -0300

    atividade de orintacao a objeto

---
+++ src/main/java/aulapratica1/PessoaApp.java
@@ -0,0 +1,21 @@
+package aulapratica1;
+
+public class PessoaApp {
+	
+
+	    
+	    public static void main(String[] args){
+	        
+	        Pessoa pessoa1 = new Pessoa(1,"Elder");
+	        
+	        Pessoa pessoa2 = new Pessoa(2,"Ana");
+	        
+	        System.out.println("o nome do objeto pessoa1 eh: "+pessoa1.getNome());
+	        
+	        pessoa1.setNome("Helder");
+	        
+	        System.out.println("o nome do objeto pessoa1 eh: "+pessoa1.getNome());
+	                
+	        
+	    }  
+}
